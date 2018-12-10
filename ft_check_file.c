d78ee0b63a72712efd614eb939b59fc446c26a4b
Pierre MICHAUD <pimichau@e3r9p4.42.fr>
2018-12-10 17:43:43 +0100

    correction with sides check

--- ft_check_file.c
+++ ft_check_file.c
@@ -40,7 +40,7 @@ char	**ft_read_file(int fd)
 	i = 0;
 	tab = NULL;
 	line = (ft_strnew(0));
-	while (ft_get_next_line(fd, &line) && i < 131)
+	while (ft_get_next_line(fd, &line) && i < 130)
 	{
 		j = 0;
 		temp = tab;
@@ -58,7 +58,7 @@ char	**ft_read_file(int fd)
 	}
 	if (i)
 		tab[i] = NULL;
-/*	i = 0;
+	i = 0;
 	printf("--------->le tableau enregistre<----------\n");
 	while (tab[i])
 	{
@@ -67,8 +67,8 @@ char	**ft_read_file(int fd)
 		i++;
 	}
 	printf("------------------>fin<-------------------\n");
-*/
-	if (i < 131)
+
+	if (i < 130)
 	 	return (tab);
 	ft_free_tab(&tab);
 	return (NULL);
@@ -78,37 +78,46 @@ int		ft_check_errors(char **tab)
 {
 	int		i;
 	int		sharp;
+	int		sides;
 
 	i = -1;
 	sharp = 0;
+	sides = 0;
 	while (tab[++i])
 	{
-		if (i > 129 || (((i + 1) % 5 != 0) && ft_strlen(tab[i]) != 4) || sharp > 4)
+		if (i > 128 || (((i + 1) % 5 != 0) && ft_strlen(tab[i]) != 4) || sharp > 4)
 			return (0);
 		//printf("1er if passe\n");
-		if (((i + 1) % 5 == 0) && ft_strlen(tab[i]) != 0)
+		if (((i + 1) % 5 == 0) && (ft_strlen(tab[i]) != 0))
 			return (0);
 		//printf("2eme if passe\n");
 		if ((i + 1) % 5 == 0)
 		{
-			if (sharp != 4)
+			if (sharp != 4 || sides < 6)
 				return (0);
 			else
+			{
 				sharp = 0;
+				sides = 0;
+			}
 		}
 		//printf("3eme if passe\n");
-		if (((i + 1) % 5 != 0) && tab[i])
-			if (!ft_check_errors_2(tab, i, &sharp))
+		if ((i + 1) % 5 != 0)
+			if (!ft_check_errors_2(tab, i, &sharp, &sides))
 				return (0);
-
+		if (tab[i + 1] == 0)
+		{
+			if (sharp != 4 || sides < 6)
+				return (0);
+		}
 		//printf("4eme if passe avec ft2\n");
 	}
-	if (i % 5 != 0)
+	if (((i - 4 ) % 5) != 0 || i <= 4)
 		return (0);
 	return (1);
 }
 
-int		ft_check_errors_2(char **tab, int i, int *sharp)
+int		ft_check_errors_2(char **tab, int i, int *sharp, int *sides)
 {
 	int j;
 	int	check;
@@ -124,16 +133,19 @@ int		ft_check_errors_2(char **tab, int i, int *sharp)
 		if (tab[i][j] == '#')
 		{
 			(*sharp)++;
-			if (((j > 0) && tab[i][j - 1] == '#') || ((j < 3) &&
-			tab[i][j + 1] == '#'))
-				check = 1;
-			if (!check && ((tab[i + 1] && tab[i + 1][0] && tab[i + 1][j] == '#') ||
-			((i > 0) &&	tab[i - 1][0] && tab[i - 1][j] == '#')))
-				check = 1;
+			if (((j > 0) && tab[i][j - 1] == '#'))
+				check += 1;
+			if ((j < 3) && tab[i][j + 1] == '#')
+				check += 1;
+			if ((tab[i + 1] && tab[i + 1][0] && tab[i + 1][j] == '#'))
+				check += 1;
+			if ((i > 0) &&	tab[i - 1][0] && tab[i - 1][j] == '#')
+				check += 1;
 			if (!check)
 				return (0);
 		}
 		//printf("ft2 -> i : %d, j : %d\n", i, j);
 	}
+	*sides = *sides + check;
 	return (1);
 }
